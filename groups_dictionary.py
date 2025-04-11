8c16d337ee1bc6dc44619e39f27aaedb07f55a32
Diloytte <mail@diloytte.com>
2025-04-11 22:45:35 +0200

    Update groups_dictionary.py

--- groups_dictionary.py
+++ groups_dictionary.py
@@ -2,26 +2,5 @@
 
 # Dictionary of group names and their IDs
 group_name_to_id = {
-    "Cookery": -1002225011125, #
-    "CrikeyGems": -1002166046392,#
-    "WBR": -1002001023272, #
-    "Cliff": -1002228238193, #
-    "Feli CTO's": -1002232151687, #
-    "CCLC": -1002451333286, #
-    "Techsol Pub": -1002229304166, #
-    "CCLab": -1002167336349, #
-    "NFG": -1001790451709, #
-    "BBK PC": -1002335127408, #
-    "Dumbo Circus": -1002264692876, #
-    "Trench Net": -1002163488444, #
-    "Kitchen": -1002318739589, #
-    "BKH Public G": -1002159178361,
-    "Kitchen Pub": -1002472494807, #
-    "T w Shark": -1002278702111,#
-    "Cartel Open": -1002187783299, #
-    "Cartel": -1002243485982, #
-    "Tombism": -1002386605422, #
-    "Trenches": -1002250343158, #
-    "Degenhub": -1002030672706, #
-    "AJM Cabal": -1002160252401 #
+
 }
