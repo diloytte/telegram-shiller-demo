e26c90012c888f597dda29bd88407251d2de5b99
Diloytte <mail@diloytte.com>
2025-04-11 22:45:00 +0200

    Update group_ids.py

--- group_ids.py
+++ group_ids.py
@@ -1,58 +1,56 @@
-# Private Trenches     ID: -1002462648308
-# Pabal = -1002412573733
-# Cookery = -1002225011125
-# CrikeyGemz = -1002166046392
-# BAKI ALPHA PRIVATE = -1002014016057 (Check Channel)
-# Wake's Board Room = -1002001023272
-# Cliff Side Cabal = -1002228238193
-# Feli CTO's = -1002232151687
-# CC Lab Cabal = -1002451333286
-# Techsol Public = -1002229304166
-# Command Center Lab = -1002167336349
-# NFG = -1001790451709
-# BBK PRIVATE CABAL = -1002335127408
-# Dumbo Circus = -1002264692876
-# Trench Network = -1002163488444
-# Kitchen = -10023187395
-# BKH Public Gambols = -1002159178361
-# Kitchen Public = -1002472494807
-# Trenching with Sharkz = -1002278702111
-# Cartel open = -1002187783299
-# Cartel = -1002243485982
-# Tombism = -1002386605422
-# The Trenches = -1002250343158
+# : -1002462648308
+#  = -1002412573733
+# = -1002225011125
+#  = -1002166046392
+#  = -1002014016057 (Check Channel)
+#  = -1002001023272
+#  = -1002228238193
+#  = -1002232151687
+#  = -1002451333286
+#  = -1002229304166
+#  = -1002167336349
+#  = -1001790451709
+#  = -1002335127408
+#  = -1002264692876
+#  = -1002163488444
+#  = -10023187395
+#  = -1002159178361
+#  = -1002472494807
+#  = -1002278702111
+#  = -1002187783299
+#  = -1002243485982
+#  = -1002386605422
+#   = -1002250343158
 
-#               Priv Trench        Cookery  #       CrikeyGems  #   WBR  #         Cliff #        Feli CTO's! #   CCLC  #         Techsol Pub #
+#              
 group_ids_1 = [-1002462648308,-1002225011125,-1002166046392,-1002001023272, -1002228238193, -1002232151687, -1002451333286, -1002229304166]
 
-#              CCLab  #         NFG  #          BBK PC #        Dumbo Circus#   Trench Net.#    Kitchen      BKH Public G.
+#            .
 group_ids_2 = [-1002167336349, -1001790451709, -1002335127408, -1002264692876, -1002163488444, -1002318739589, -1002159178361]
 
-#             Kitchen Pub.     T w Shark        Cartel Open     Cartel         Tombism        Trenches        #AJM Cabal
 group_ids_3 = [-1002472494807, -1002278702111, -1002187783299, -1002243485982,-1002386605422, -1002250343158, -1002160252401]
 
-#                   BBK PC           Cliff         CCLC          
+#                
 priority_groups_1 = [-1002335127408,-1002228238193,-1002451333286]
 
-#  Cartel           Cartel 2
-#  -1002243485982, -1002187783299
+ 
 
-#                     Cookery        WBR            CCLab          Kitchen
+#              
 priority_groups_2 = [-1002225011125,-1002001023272,-1002167336349,-1002318739589]
 
-#                     Tombism        With Sharkz    Trenches       Kitchen pub
+#                 
 priority_groups_3 = [-1002386605422,-1002278702111,-1002250343158,-1002472494807]
 
-#                     CrikeyGems     Feli CTO's    NFG             Techsol pub
+#                   
 priority_groups_4 = [-1002166046392,-1002232151687,-1001790451709,-1002229304166]
 
-#                     Dumbo C        Trench N     ,  #AJM Cabal    DegenHub
+#                     
 priority_groups_5 = [-1002264692876,-1002163488444,-1002160252401,-1002030672706]  
 
-#                     Gake's B.      Lynk           Degem Cabal    Gambolers
+#                   
 priority_groups_6 = [-1002019959139,-1002170346063,-1002289207641,-1002092585078]
 
-#                     40's L
+#                    
 priority_groups_7 = [-1002222328448]
 
 
